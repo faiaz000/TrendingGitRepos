85f73964366ca3b8736d5d7247d635a666eee655
faiaz000 <faiaz000@hotmail.com>
2023-11-11 18:19:40 +0100

    git repo components added with basic UI template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RepoListComponent } from './repo-list/repo-list.component';
+import { RepoItemComponent } from './repo-list/repo-item/repo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from  '@angular/material/card'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoListComponent,
+    RepoItemComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

2c906e289c023205ca31a67906947745fd1d3933
faiaz000 <faiaz000@hotmail.com>
2023-11-12 01:47:25 +0100

    Update loadRepos method to map API response

--- src/app/repo-list/repo-list.component.ts
+++ src/app/repo-list/repo-list.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { ViewChild } from '@angular/core';
 import { GitReposService } from '../services/git-repos.service';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-repo-list',
@@ -9,27 +12,28 @@ import { GitReposService } from '../services/git-repos.service';
   styleUrls: ['./repo-list.component.scss']
 })
 export class RepoListComponent {
-  repos:any[] = [];
+  repos$: Observable<any[]>;
+  private destroy$ = new Subject<void>();
   constructor(private gitReposService: GitReposService) { } 
   totalCount = 0;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit(): void {
-    this.paginator.page.subscribe(() => {
-      this.loadRepos(this.paginator.pageIndex + 1);
+    this.paginator.page.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.repos$ = this.loadRepos(this.paginator.pageIndex + 1);
     });
-
-    this.loadRepos();
+  
+    this.repos$ = this.loadRepos();
   }
   
-  loadRepos(page: number = 1): void {
-    this.gitReposService.getMostStarredRepos(String(page)).subscribe(data => {
-      if (data) {
-        this.repos = data.items;
-        this.totalCount = data.total_count;
-      }
-    });
+  loadRepos(page: number = 1): Observable<any[]> {
+    return this.gitReposService.getMostStarredRepos(String(page)).pipe(
+      map(response => {
+        this.totalCount = response.total_count;
+        return response.items;
+      })
+    );
   }
-
+  
 }
